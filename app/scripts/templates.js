39450656940565c3f7771db9dd65dc078494f3d6
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-02 15:20:16 -0500

    adds template cache

---
+++ app/scripts/templates.js
@@ -0,0 +1,2 @@
+angular.module("blog.templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("views/week-detail.tpl.html","");
+$templateCache.put("views/week-list.tpl.html","");}]);
