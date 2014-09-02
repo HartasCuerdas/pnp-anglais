45852b85ef53f8fc3c904dfe9d720e99dba6cc73
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-02 15:39:52 -0500

    ignores generated template

--- app/scripts/templates.js
+++
@@ -1,2 +0,0 @@
-angular.module("blog.templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("views/week-detail.tpl.html","");
-$templateCache.put("views/week-list.tpl.html","");}]);
