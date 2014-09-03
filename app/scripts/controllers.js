edc24a2cc62e5ae87600986c2d47923b672a20f6
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-03 10:55:27 -0500

    testig controls to delete

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -5,7 +5,9 @@
   angular
     .module('app.controllers', ['app.services'])
     .controller('WeekListCtrl', WeekListCtrl)
-    .controller('WeekDetailCtrl', WeekDetailCtrl);
+    .controller('WeekDetailCtrl', WeekDetailCtrl)
+    .controller('WokCtrl', WokCtrl)
+    .controller('WeekDeleteCtrl', WeekDeleteCtrl);
 
   /* @ngInject */
   function WeekListCtrl (Weeks) {
@@ -15,6 +17,16 @@
   /* @ngInject */
   function WeekDetailCtrl ($routeParams, Weeks) {
     this.week = Weeks.get({ id: $routeParams.id });
+  }
+
+  /* @ngInject */
+  function WeekDeleteCtrl ($routeParams, Weeks) {
+    Weeks.remove({ id: $routeParams.id });
+  }
+
+  /* @ngInject */
+  function WokCtrl ($routeParams, Wok) {
+    Wok.delete({ id: $routeParams.id, authenticity_token: 'T/zjNTdVoWGocrCbEylftiIU4Uc+q5ZtTZaoOH35gqc=', _method: 'delete' });
   }
 
 
