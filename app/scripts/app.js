e31ef99a30e8ea6fe4257fa9395fafc31b2f7f9a
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-07 17:42:08 -0500

    enough paths

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,17 +16,17 @@
 
     $routeProvider
       .when('/', {
-        templateUrl: '/views/week-list.tpl.html',
+        templateUrl: 'views/week-list.tpl.html',
         controller: 'WeekListCtrl',
         controllerAs: 'weeklist'
       })
       .when('/:id', {
-        templateUrl: '/views/week-detail.tpl.html',
+        templateUrl: 'views/week-detail.tpl.html',
         controller: 'WeekDetailCtrl',
         controllerAs: 'weekdetail'
       })
       .when('/day/:id', {
-        templateUrl: '/views/day-detail.tpl.html',
+        templateUrl: 'views/day-detail.tpl.html',
         controller: 'DayDetailCtrl',
         controllerAs: 'daydetail'
       });
