fe5fbc785584e9942cf7ce37ea4517f20d01b5a5
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-25 17:22:35 -0500

    adds toggle_o/d functions

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -41,10 +41,30 @@
   }
 
   /* @ngInject */
-  function DayDetailCtrl ($routeParams, Day, Ods, $scope, Page) {
+  function DayDetailCtrl ($routeParams, Day, Ods, OdToggleO, OdToggleD, $scope, Page) {
     Page.setTitle('Day');
     this.day = Day.get({ id: $routeParams.id });
     this.ods = Ods.query({ id: $routeParams.id });
+    $scope.toggle_o = function(odId, position) {
+      (OdToggleO.toggle_o({ id: odId }))
+        .$promise
+          .then(function(data) {
+            $scope.daydetail.ods[position].o = !$scope.daydetail.ods[position].o;
+          })
+          .catch(function(response) {
+            console.log(response);
+          });
+    };
+    $scope.toggle_d = function(odId, position) {
+      (OdToggleD.toggle_d({ id: odId }))
+        .$promise
+          .then(function(data) {
+            $scope.daydetail.ods[position].d = !$scope.daydetail.ods[position].d;
+          })
+          .catch(function(response) {
+            console.log(response);
+          });
+    };
   }
 
   /* @ngInject */
