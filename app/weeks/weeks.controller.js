776aadf7a181a3f8e21109cf44c94252b7ddef1b
Franco Cedillo <franco.cedillo@gmail.com>
2014-10-03 00:24:07 -0500

    refactors create weeks in a directive

--- app/weeks/weeks.controller.js
+++ app/weeks/weeks.controller.js
@@ -20,16 +20,6 @@
 
     $scope.currentWeekFirstDayDate = currentWeekFirstDayDate();
 
-    $scope.createWeek = function() {
-      var week = new Week();
-      week.$save(
-        {},
-        function(){
-          $scope.weeklist.weeks.push(week);    
-        }
-      );
-    }
-
     $scope.destroyWeek = function(weekId, position) {
       Week.delete(
         { id: weekId },
--- app/weeks/weeks.directives.js
+++ app/weeks/weeks.directives.js
@@ -3,6 +3,28 @@
 
   /* @ngInject */
   angular
-    .module('app.controllers');
+    .module('app.controllers')
+    .directive('pnpWeekCreate', ['Week', pnpWeekCreate]);
+
+  /* @ngInject */
+  function pnpWeekCreate(Week) {
+    return {
+      scope: {
+        weeks: '=pnpWeekCreate'
+      },
+      restrict: "A",
+      link: function(scope, element, attrs ) {
+        element.bind("click", function() {
+          var week = new Week();
+          week.$save(
+            {},
+            function(){
+              scope.weeks.push(week);    
+            }
+          );
+        });
+      }
+    }
+  }
 
 })();
